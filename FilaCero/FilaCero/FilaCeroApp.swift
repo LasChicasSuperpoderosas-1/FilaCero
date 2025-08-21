7cb787bb245214c9094dafc824661a976f288536
Diego <saldana_d@outlook.com>
2025-08-21 10:12:12 -0600

    Archivo de FilaCero

---
+++ FilaCero/FilaCero/FilaCeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilaCeroApp.swift
+//  FilaCero
+//
+//  Created by Diego Saldaña on 21/08/25.
+//
+
+import SwiftUI
+
+@main
+struct FilaCeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

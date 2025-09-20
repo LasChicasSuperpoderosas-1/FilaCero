bff39d724ba27ce338c208ff344217984dec99c3
Diego <saldana_d@outlook.com>
2025-09-20 13:31:52 -0600

    Css File y TurnoEspecial
    Cree un folder para poner diseños y componentes para no tenerlos en el codigo, como un CSS File. También puse mi pantalla de TurnoEspecial, nomas que hay que borrar la carpeta de testing porque no es necesaria

---
+++ FilaCero/FilaCero/Components/Brand.swift
@@ -0,0 +1,18 @@
+//
+//  Brand.swift
+//  FilaCero
+//
+//  Created by Diego Saldaña on 20/09/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct Brand {
+        static let primary = Color(red: 1/255, green: 104/255, blue: 138/255)        // #01688A
+        static let textPrimary  = Color(red: 51/255,  green: 51/255,  blue: 51/255)  // #333333
+        static let textSecondary = Color(red: 102/255, green: 102/255, blue: 102/255)// #666666
+        static let appBackground = Color(red: 242/255, green: 242/255, blue: 242/255)// #F2F2F2
+        static let cardRadius: CGFloat = 16
+        static let buttonHeight: CGFloat = 56
+}
--- FilaCero/FilaCero/Views/InicioView.swift
+++ FilaCero/FilaCero/Views/InicioView.swift
@@ -7,15 +7,6 @@
 
 import SwiftUI
 
-enum Brand {
-    static let primary = Color(red: 1/255, green: 104/255, blue: 138/255)        // #01688A
-    static let textPrimary  = Color(red: 51/255,  green: 51/255,  blue: 51/255)  // #333333
-    static let textSecondary = Color(red: 102/255, green: 102/255, blue: 102/255)// #666666
-    static let appBackground = Color(red: 242/255, green: 242/255, blue: 242/255)// #F2F2F2
-    static let cardRadius: CGFloat = 16
-    static let buttonHeight: CGFloat = 56
-}
-
 struct InicioView: View {
     var onGenerateTurn: () -> Void = {}
     var onSignOut: () -> Void = {}

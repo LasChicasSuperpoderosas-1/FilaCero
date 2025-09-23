3d5447771f7f4cf55589331d8dcdd632260735e6
Emilio Puga Ascencio <emilio.puga@live.com>
2025-09-23 10:22:24 -0600

    Mi pantalla de front terminada sin back completamente terminado
    Mi pantalla de front terminada sin back completamente terminado

--- FilaCero/FilaCero/FilaCeroApp.swift
+++ FilaCero/FilaCero/FilaCeroApp.swift
@@ -4,14 +4,15 @@
 //
 //  Created by Diego Saldaña on 21/08/25.
 //
-
 import SwiftUI
-
 @main
 struct FilaCeroApp: App {
+    init() {
+        VentanillaHabDesAPI.configurePin(certName: "tc2007b", ext: "cer")
+    }
+
     var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+        WindowGroup { ContentView() }
     }
 }
+
